70a410969c72bd16453ecbeba4443c8b205cdf98
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-08-05 22:12:10 -0600

    Dynamic input width

--- src/components/Testing/TestingContent.jsx
+++ src/components/Testing/TestingContent.jsx
@@ -18,17 +18,23 @@ const TestingContent = ({ placeholder, value, setValue }) => {
       </div>
       <div className="testing__container">
         <div className="testing__content">
-          <div className="testing__input--container">
-            <p className="testing__content--para">click to type</p>
-            <input
-              value={value}
-              name={placeholder}
-              className="testing__content--input"
-              placeholder={placeholder}
-              type="text"
-              onChange={(e) => setValue(e.target.value)}
-              spellCheck={false}
-            />
+          {/* TODO: Should this be a <form>? */}
+          <p className="testing__content--para">click to type</p>
+          <div className="testing__input--wrapper">
+            <div className="testing__input--container">
+              <span className="testing__content--span">
+                {value || placeholder}
+              </span>
+              <input
+                value={value}
+                name={placeholder}
+                className="testing__content--input"
+                placeholder={placeholder}
+                type="text"
+                onChange={(e) => setValue(e.target.value)}
+                spellCheck={false}
+              />
+            </div>
           </div>
         </div>
       </div>
