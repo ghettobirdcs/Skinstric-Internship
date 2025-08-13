885f8bda1233982af1d3531ab8757c09a90e14df
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-08-13 02:04:48 -0600

    Implemented react-circular-progressbar

--- src/components/Summary/SummaryContent.jsx
+++ src/components/Summary/SummaryContent.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import "./SummaryContent.css";
 
 const SummaryContent = ({ type, title, value }) => {
+  const titleClass =
+    type === "Gender"
+      ? "summary__title--uppercase"
+      : type === "Race"
+        ? "summary__title--capitalize"
+        : "";
+
+  const progressStyles = useMemo(
+    () =>
+      buildStyles({
+        pathColor: "#1A1B1C",
+        trailColor: "#E1E1E2",
+        textColor: "#1A1B1C",
+        textSize: "40px",
+        strokeLinecap: "round",
+        pathTransitionDuration: 0.6,
+      }),
+    [],
+  );
+
   return (
     <div className="summary__content">
-      <span
-        className={`summary__title ${type === "Gender" ? "summary__title--uppercase" : type === "Race" ? "summary__title--capitalize" : ""}`}
-      >
+      <span className={`summary__title ${titleClass}`}>
         {title} {type === "Age" && "y.o."}
       </span>
-      {/* TODO: Learn how to make a circular percentile display */}
+
       <div className="summary__circle">
-        <span className="summary__circle--value">{value}</span>
-        <span className="summary__percentage">%</span>
+        <CircularProgressbar
+          value={value}
+          strokeWidth={1}
+          styles={progressStyles}
+        />
+        <div className="summary__circle__overlay">
+          <span className="summary__circle--value">{value}</span>
+          <span className="summary__percentage">%</span>
+        </div>
       </div>
     </div>
   );
